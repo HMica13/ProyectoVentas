238da4e8aa7d191d7834c865f492b075942b92f8
Micaela Cueva <tuemail@ejemplo.com>
2025-07-14 23:25:20 -0500

    ÚltimaTarea

---
+++ vista/js/categoria.js
@@ -0,0 +1,38 @@
+var tabla;
+
+//Creamos una funcion int que se ejecuta al inicio de la aplicación
+function init(){
+    mostrarform(false);
+}
+
+//Función para limpiar el formulario
+function limpiar(){
+    $("#idcategoria").val("");
+    $("#nombre").val("");
+    $("#descripcion").val("");
+
+}
+
+//Función para mostrar el formulario
+function mostrarform(flag){
+    limpiar();
+    if(flag){
+        $("#listadoregistros").hide();
+        $("#formularioregistros").show();
+        $("#btnGuardar").prop("disabled", false);
+        $("#btnagregar").hide();
+    }else{
+        $("#listadoregistro").show();
+        $("#formularioregistros").hide();
+        $("#btnagregar").show();
+    }
+}
+
+//Función para cancelar el formulario
+function cancelarForm(){
+    limpiar();
+    mostrarform(false);
+}
+
+//Ejecutamos la función init
+init();
